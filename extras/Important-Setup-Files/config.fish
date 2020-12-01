2cb09d083a5ded9616b492f1609665ed5ef43df8
ZafarAqil <Zafaraqil14@gmail.com>
2020-12-01 17:19:09 +0530

    i3 dotfiles

---
+++ extras/Important-Setup-Files/config.fish
@@ -0,0 +1,6 @@
+#set -xg PYTHONPATH /home/zafaraqil/.local/bin/ $PYTHONPATH 
+set PATH /home/zafaraqil/.local/bin/ $PATH
+
+alias nf='neofetch'
+alias dl="aria2c -s 16 -x 16"
+alias tb="nc termbin.com 9999"
