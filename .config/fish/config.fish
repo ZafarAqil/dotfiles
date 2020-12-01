c45055f3a5b4bafa615a783e1b4dc2c84718862a
ZafarAqil <Zafaraqil14@gmail.com>
2020-12-01 17:08:49 +0530

    i3 dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+# Helps to make fish shell read pip packages installed locally
+#set -xg PYTHONPATH /home/zafaraqil/.local/bin/ $PYTHONPATH 
+set PATH /home/zafaraqil/.local/bin/ $PATH
+
+alias nf='neofetch'
+alias dl="aria2c -s 16 -x 16"
+alias tb="nc termbin.com 9999"
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/zafar/.local/share/omf/themes/agnoster/fish_prompt.fish
